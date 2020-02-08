c37b88403a4874d8e00305f6f0e9ada2cad9d5e2
maram-ali-ghilani <marrr062@gmail.com>
2020-02-08 22:53:37 +0300

    create game logic by if condition

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,27 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
+
+
+//alert('Hello, friends.');
+//Arrays
+const cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+
+// Variables
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//Print
+console.log("User flipped1 " + cardsInPlay[0]);
+console.log("user flipped 2" + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0]===cardsInPlay[1]){
+    alert('you find a Match !');
+  }else{
+    alert('Sorry, tray again ..')
+  }
+}
