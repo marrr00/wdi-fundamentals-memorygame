bbe7897553636cf9770669d59605b19b93a34c1a
maram-ali-ghilani <marrr062@gmail.com>
2020-02-08 23:23:29 +0300

    create the logic of the game by using function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,20 +8,25 @@ console.log("Up and running!");
 const cards = ["queen","queen","king","king"];
 let cardsInPlay = [];
 
-// Variables
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function checkForMatch(){
+  //if condition
+    if(cardsInPlay[0]===cardsInPlay[1]){
+      alert('you find a Match !');
+    }else{
+      alert('Sorry, tray again ..')
+    }
+}//function
 
-//Print
-console.log("User flipped1 " + cardsInPlay[0]);
-console.log("user flipped 2" + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
-  if(cardsInPlay[0]===cardsInPlay[1]){
-    alert('you find a Match !');
-  }else{
-    alert('Sorry, tray again ..')
+//*******************************
+function flipCard(cardId){
+  //add to array .. push
+  cardsInPlay.push(cards[cardId]);
+  //print
+  console.log("User flipped " + cards[cardId]);
+  if(cardsInPlay.length === 2){
+     checkForMatch();
   }
-}
+}//function
+
+flipCard(0);
+flipCard(2);
